2a538c85a21b0621f6011f68a77a89a0dc7b2c88
Emre Dogan <emre.dogan.1@protonmail.com>
2023-05-13 06:32:44 +0100

    Created Additive versions of load handlers.

---
+++ Editor/MainSceneLoadedHandlers/LoadActiveSceneAdditive.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Ems.MainSceneAutoLoading.Utilities;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+namespace Ems.MainSceneAutoLoading.MainSceneLoadedHandlers
+{
+    public class LoadActiveSceneAdditive : IMainSceneLoadedHandler
+    {
+        public void OnMainSceneLoaded(LoadMainSceneArgs args)
+        {
+            var path = args.SceneSetups.First(scene => scene.isActive).path;
+            SceneManager.LoadScene(path, LoadSceneMode.Additive);
+            SceneHierarchyStateUtility.StartRestoreHierarchyStateCoroutine(args);
+        }
+        
+        [CustomPropertyDrawer(typeof(LoadActiveSceneAdditive))]
+        public sealed class Drawer : BasePropertyDrawer
+        {
+            public override string Description =>
+                $"Loads only one scene that was active(with bold name) in hierarchy before entering playmode.";
+        }
+    }
+}
